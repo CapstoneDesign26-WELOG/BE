d8f38dc96703eeef68b42cd1d89ac3f8a40c4bc8
kushinada2077 <kushinada2077@gmail.com>
2026-05-05 16:41:31 +0900

    feat: UserRepository 구현

---
+++ internal/user/repository.go
@@ -0,0 +1,32 @@
+package user
+
+import (
+	"errors"
+	"welog/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{DB: db}
+}
+
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	var user model.User
+	err := r.DB.Where("email = ?", email).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *UserRepository) Create(user *model.User) error {
+	return r.DB.Create(user).Error
+}
