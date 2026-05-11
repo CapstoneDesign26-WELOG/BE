0d3ff1af5fcaddcb301d90a914dbe715c4e73294
kushinada2077 <kushinada2077@gmail.com>
2026-05-11 19:16:30 +0900

    feat: comment 도메인 repository 구현

--- internal/comment/repository.go
+++ internal/comment/repository.go
@@ -20,18 +20,33 @@ func (r *CommentRepository) Create(comment *model.Comment) error {
 	return r.db.Create(comment).Error
 }
 
-func (r *CommentRepository) FindAllByPostID(postID uint) ([]model.Comment, error) {
-	var comments []model.Comment
-	query := r.db.Model(&model.Comment{}).Where("post_id = ?", postID)
-
-	err := query.Order("created_at desc").Find(&comments).Error
-	return comments, err
+func (r *CommentRepository) FindByID(commentID uint) (*model.Comment, error) {
+	var comment model.Comment
+	err := r.db.First(&comment, commentID).Error
+	if err != nil {
+		return nil, err
+	}
+	return &comment, nil
 }
 
-func (r *CommentRepository) FindAllByUserID(userID uint) ([]model.Comment, error) {
-	var comments []model.Comment
-	query := r.db.Model(&model.Comment{}).Where("user_id = ?", userID)
+func (r *CommentRepository) Delete(commentID uint) error {
+	return r.db.Delete(&model.Comment{}, commentID).Error
+}
 
-	err := query.Order("created_at desc").Find(&comments).Error
-	return comments, err
+func (r *CommentRepository) UpsertPreference(userID, aiType uint, score int) error {
+	var pref model.UserPreference
+	err := r.db.Where("user_id = ? AND ai_type = ?", userID, aiType).First(&pref).Error
+
+	if err == gorm.ErrRecordNotFound {
+		pref = model.UserPreference{
+			UserID: userID,
+			AIType: aiType,
+			Score:  score,
+		}
+		return r.db.Create(&pref).Error
+	} else if err != nil {
+		return err
+	}
+
+	return r.db.Model(&pref).Update("score", gorm.Expr("score + ?", score)).Error
 }
