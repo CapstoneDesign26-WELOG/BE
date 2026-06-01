73edb1fe8936a5f44b7ab3416eee6a2cac51cd12
kushinada2077 <kushinada2077@gmail.com>
2026-06-01 15:15:28 +0900

    fix: /api/users/me 응답에 userID 추가

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -33,6 +33,7 @@ func (h *UserHandler) GetMe(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]interface{}{
+		"user_id":     user.ID,
 		"email":       user.Email,
 		"token_count": user.TokenCount,
 	})
